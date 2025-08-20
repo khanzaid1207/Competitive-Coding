34104f389f1bf59efb929f525df01f2d5ea285f8
Zaid Khan <khanzaid1207@gmail.com>
2025-08-20 08:55:40 +0530

    Add files via upload

---
+++ min_rotated_sorted_array.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int findMin(int arr[], int n) {
+    int low = 0, high = n - 1;
+
+    while (low < high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] > arr[high]) {
+            low = mid + 1;
+        } else {
+            high = mid;
+        }
+    }
+
+    return arr[low];
+}
+
+int main() {
+    int arr[] = {4, 5, 6, 7, 0, 1, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Minimum element is: " << findMin(arr, n) << endl;
+    return 0;
+}
